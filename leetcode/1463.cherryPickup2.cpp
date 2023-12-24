9e4512cd26e1611473bfb58a49dc7c44594de491
GinOwO <65128789+GinOwO@users.noreply.github.com>
2023-12-25 01:07:18 +0530

    Cherry pickup

---
+++ leetcode/1463.cherryPickup2.cpp
@@ -0,0 +1,99 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/*
+    LeetCode 1463 - Cherry Pickup II
+        2 Robots, start from top left and top right in a maze of size n x m. Collect maximum cherries. No obstacles
+        Robots can move to the next row in 3 ways: left, right, or stay in the same column
+        Use a 3D memo. As both are moving in the same row, it means r1==r2, using this we can eliminate the 4D
+        Hence just go through all possible 9 combos: r+1, c+{-1, 0, 1} and take max of all
+        If it goes out of bounds, return -1e9
+        If on last row, if both on same cell, return grid[r][c1], else return grid[r][c1]+grid[r][c2]
+        At every recursion, take cherry = grid[r][c1]+grid[r][c2]. To prevent double counting, if both on same cell do cherry = grid[r][c1]
+        Return max of all 9 combos + cherry
+        Time: O(n^3), Space: O(n^3)
+    
+    Tabulation:
+        3D -> 2D, using prev, current. Let dp, ndp of size mxm
+        Base case is last row, copy that to dp.
+        Since last row is already filled, start from r=n-2
+        Let r=n-1->0, c1=0->m-1, c2=0->m-1 || inclusive
+        At every cell, take cherry = grid[r][c1]+grid[r][c2]. To prevent double counting, if both on same cell do cherry = grid[r][c1]
+
+        Let i=-1->1, j=-1->1 || inclusive
+        If c1, c2 are in bounds, take mx = max(mx, dp[c1+i][c2+j])
+
+        Let ndp[c1][c2] = mx+cherry, which is the max of all 9 combos + cherry
+
+        Then dp = ndp, this is done inside c2 loop
+
+        Return dp[0][m-1] as ans is stored here and not in dp[0][0]
+
+*/
+
+class Solution {
+public:
+    int cherryPickup2ManMemo(const vector<vector<int>>& grid, const int m, int r, int c1, int c2, int*** memo2P){
+        if(c1<0|| c2<0 || c1>=m || c2>=m) return -1e8;
+        if(r==grid.size()-1){
+            if(c1==c2) return grid[r][c1];
+            else return grid[r][c1]+grid[r][c2];
+        }
+
+        if(memo2P[r][c1][c2]!=-1) return memo2P[r][c1][c2];
+
+        int ans=0, x=-1e8;
+        if(c1==c2) ans = grid[r][c1];
+        else ans = grid[r][c1] + grid[r][c2];
+
+        for(int i=-1; i<2; i++)
+            for(int j=-1; j<2; j++)
+                x = max(x, cherryPickup2ManMemo(grid, m, r+1, c1+i, c2+j, memo2P));
+
+        ans+=x;
+        return memo2P[r][c1][c2] = ans;
+    }
+
+    int cherryPickup(vector<vector<int>>& grid) {
+        const int n = grid.size(), m = grid[0].size();
+
+        // memset(memo2P, -1, 71*71*71*sizeof(int));
+        // int memo2P[71][71][71];
+        // return cherryPickup2ManMemo(grid, m, 0, 0, m-1);
+
+        vector<vector<int>>  dp(m, vector<int>(m, 0));
+        vector<vector<int>> ndp(m, vector<int>(m, 0));
+        for(int i=0; i<m; i++){
+            for(int j=0; j<m; j++){
+                if(i==j) dp[i][j] = grid[n-1][j];
+                else dp[i][j] = grid[n-1][i]+grid[n-1][j];
+            }
+        }
+
+        for(int r=n-2; r>=0; r--){
+            for(int c1=0; c1<m; c1++){
+                for(int c2=0; c2<m; c2++){
+                    int mx = -1e9, ans;
+                    if(c1==c2) ans = grid[r][c1];
+                    else ans = grid[r][c1] + grid[r][c2];
+
+                    for(int i=-1; i<2; i++)
+                        for(int j=-1; j<2; j++)
+                            if(c1+i>=0 && c2+j>=0 && c1+i<m && c2+j<m) 
+                                mx = max(mx, dp[c1+i][c2+j]);
+                    
+                    ndp[c1][c2] = mx+ans;
+                }
+            }
+            dp = ndp;
+        }
+        return dp[0][m-1];
+    }
+};
+
+int main(){
+    vector<vector<int>> grid = {{0,1,-1},{1,0,-1},{1,1,1}};
+    cout << Solution().cherryPickup(grid) << endl;
+    return 0;
+}
---
+++ leetcode/741.cherryPickup.cpp
@@ -0,0 +1,94 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/*
+    Leetcode 741 - Cherry Pickup
+        Go from top left to bottom right, and then bottom right to top left and collect maximum cherries
+        Optimization is to consider the fact that moving one person from top left to bottom right and back is the same as moving 2 people from top left to bottom right
+        Use a 3D memo. As both are moving together, it means r1+c1 = r2+c2, using this we can eliminate the 4D
+        Hence just go through all possible directions, i.e vertical and horizontal: vv, hh, hv, vh and take max of all
+        If going out of bounds or hitting a thorn, return -1e9
+        If one person reaches bottom right, return the value at that cell
+        At every recursion, take cherry = grid[r1][c1]+grid[r2][c2]. To prevent double counting, if both on same cell do cherry = grid[r1][c1]
+        Return max of all 4 directions + cherry
+        Time: O(n^3), Space: O(n^3)
+*/
+
+class Solution {
+private:
+    // r1+c1==r2+c2, hence get 3d map using c2 = r1+c1-r2
+    // int memo2P[50][50][50] = {-1}; // with a bit more changes
+    map<pair<pair<int,int>, int>, int> memo2P;
+    int cherryPickup2ManMemo(vector<vector<int>>& grid, const int n, int r1=0, int c1=0, int r2=0){
+        if(memo2P.count({{r1,c1}, r2})) return memo2P[{{r1,c1}, r2}];
+        
+        int c2 = r1+c1-r2;
+        if(r1>=n || c1>=n || r2>=n || c2>=n || grid[r1][c1]==-1 || grid[r2][c2]==-1) return -1e9;
+        if(r1==n-1 && c1==n-1) return grid[r1][c1];
+
+        int ans=0;
+        if(r1==r2 && c1==c2) ans = grid[r1][c1];
+        else ans = grid[r1][c1] + grid[r2][c2];
+
+        int hh = cherryPickup2ManMemo(grid, n, r1, c1+1, r2);
+        int hv = cherryPickup2ManMemo(grid, n, r1, c1+1, r2+1);
+        int vh = cherryPickup2ManMemo(grid, n, r1+1, c1, r2);
+        int vv = cherryPickup2ManMemo(grid, n, r1+1, c1, r2+1);
+
+        ans+=max(max(vv, hh), max(hv, vh));
+        return memo2P[{{r1,c1}, r2}] = ans;
+    }
+
+    int cherryPickup2Man(vector<vector<int>>& grid, const int n, int r1=0, int c1=0, int r2=0, int c2=0){
+        if(r1>=n || c1>=n || r2>=n || c2>=n || grid[r1][c1]==-1 || grid[r2][c2]==-1) return -1e9;
+        if(r1==n-1 && c1==n-1) return grid[r1][c1];
+
+        int ans=0;
+        if(r1==r2 && c1==c2) ans = grid[r1][c1];
+        else ans = grid[r1][c1] + grid[r2][c2];
+
+        int hh = cherryPickup2Man(grid, n, r1, c1+1, r2, c2+1);
+        int hv = cherryPickup2Man(grid, n, r1, c1+1, r2+1, c2);
+        int vh = cherryPickup2Man(grid, n, r1+1, c1, r2, c2+1);
+        int vv = cherryPickup2Man(grid, n, r1+1, c1, r2+1, c2);
+
+        ans+=max(max(vv, hh), max(hv, vh));
+        return ans;
+    }
+
+    vector<map<pair<int,int>, int>> memo{{},{}};
+    int cherryPickupRecMemo(vector<vector<int>>& grid, const int n, const int i=0, const int j=0, int dir=1){
+        if(n==1) return grid[0][0];
+        if(memo[dir<0?0:1].count({i,j})) return memo[dir==1?0:-1][{i,j}];
+        if(dir == 1 && i==n-1 && j==n-1) return cherryPickupRec(grid, n, i, j, -1);
+        if(dir == -1 && i==0 && j==0) return 0;
+        if(i<0 || j<0 || i>=n || j>=n || grid[i][j]==-1) return -1e9;
+        int q = grid[i][j]; grid[i][j]=0;
+        int ans = max(cherryPickupRec(grid, n, i+dir, j, dir), cherryPickupRec(grid, n, i, j+dir, dir))+q;
+        grid[i][j] = q;
+        return memo[dir<0?0:1][{i,j}]=ans;
+    }
+
+    int cherryPickupRec(vector<vector<int>>& grid, const int n, const int i=0, const int j=0, int dir=1){
+        if(n==1) return grid[0][0];
+        if(dir == 1 && i==n-1 && j==n-1) return cherryPickupRec(grid, n, i, j, -1);
+        if(dir == -1 && i==0 && j==0) return 0;
+        if(i<0 || j<0 || i>=n || j>=n || grid[i][j]==-1) return -1e9;
+        int q = grid[i][j]; grid[i][j]=0;
+        int ans = max(cherryPickupRec(grid, n, i+dir, j, dir), cherryPickupRec(grid, n, i, j+dir, dir))+q;
+        grid[i][j] = q;
+        return ans;
+    }
+
+public:
+    int cherryPickup(vector<vector<int>>& grid){
+        return max(cherryPickup2ManMemo(grid, grid.size()), 0);
+    }
+};
+
+int main(){
+    vector<vector<int>> grid = {{0,1,-1},{1,0,-1},{1,1,1}};
+    cout << Solution().cherryPickup(grid) << endl;
+    return 0;
+}
