aaaea7f46e886afcfaba6cb430e5a2985bae3515
GinOwO <65128789+GinOwO@users.noreply.github.com>
2023-12-30 18:37:47 +0530

    fix ci again

--- html/scripts/buildURI.sh
+++ html/scripts/buildURI.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-echo "Getting Metadata from Leetcode"
-node html/scripts/buildMetadata.js
-echo "Received Metadata"
-echo ""
+# echo "Getting Metadata from Leetcode"
+# node html/scripts/buildMetadata.js
+# echo "Received Metadata"
+# echo ""
 
 echo "Building URIs"
 mapfile -d $'\0' cppfiles < <(find . -name "*.cpp" -print0)
@@ -13,4 +13,4 @@ echo ""
 
 echo "Cleanup"
 mv html/scripts/URIs.json ./URIs.json
-rm html/scripts/metadata.json
+# rm html/scripts/metadata.json
